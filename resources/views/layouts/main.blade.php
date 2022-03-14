70b2897e6d13e7a93b354957fb51573d5ce66141
StanislavKls <klecov@gmail.com>
2022-03-14 15:15:28 +0500

    Breeze to bootstrap

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-param" content="_token" />
+
+    <title>{{ config('app.name') }} - @yield('title')</title>
+
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap core CSS -->
+    <!-- CSS only -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    </head>
+<body>
+    <div id="app">
+        @include('layouts.nav')
+
+        <main class="container py-4">
+            <div class="container">
+                @yield('content')
+            </div>
+        </main>
+    </div> 
+</body>
+</html>
